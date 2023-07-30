acb2d77403b4e954bab8eb78020f88d127c3d010
Flickka Da Swizzlnist <114291178+SirFlickka@users.noreply.github.com>
2023-07-30 10:53:21 -0400

    board.py

--- src/board.py
+++ src/board.py
@@ -1,7 +1,9 @@
 # board.py
-class Board:
+        class QuantumBoard:
     def __init__(self):
-        # TODO: Initialize a quantum chess board
+        self.state = QuantumRegister(384)  # example for the separate registers approach
+        self.circuit = QuantumCircuit(self.state)
 
+    # Methods to move pieces, check the state of the board, etc
     def make_move(self, move):
         # TODO: Implement making a move on the board
